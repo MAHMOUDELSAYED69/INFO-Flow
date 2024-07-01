30420c7e53c994388d677a7fe3a9b73ae42e3baa
MahmoudElSayed96 <gcfjxvkj@gmail.com>
2024-07-01 05:11:09 +0300

    add font assets, create ColorManager class, add extention folder.

---
+++ lib/constant/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class ColorManager {
+  static const Color white = Colors.white;
+  static Color yellow = Colors.yellow[800]!;
+  static const Color black = Colors.black;
+  static const Color grey = Colors.grey;
+  static const Color blue = Colors.blue;
+}
