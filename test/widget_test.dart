a85102f8da4288a3f43f3e8e8c3811b8ca84a829
MahmoudElSayed96 <gcfjxvkj@gmail.com>
2024-07-01 11:12:45 +0300

    change the image assets.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,13 +7,13 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:projectx/main.dart';
+import 'package:infoflow/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
+
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
